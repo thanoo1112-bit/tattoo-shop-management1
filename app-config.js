51afa3d9da5b13d9e1945f6de53159b59ba3a48c
thanoo1112-bit <thanoo.1112@gmail.com>
2026-07-16 15:09:57 +0700

    Update Supabase URL and Anon Key in app-config.js

--- app-config.js
+++ app-config.js
@@ -6,8 +6,8 @@
  * 2. หากยังไม่มีระบบจะบันทึกข้อมูลและดึงข้อมูลผ่าน LocalStorage โดยอัตโนมัติ (ช่วยให้ทดสอบหน้าร้านได้ทันที!)
  */
 
-const SUPABASE_URL = ""; // ป้อน Supabase Project URL ที่นี่ เช่น "https://xxxx.supabase.co"
-const SUPABASE_ANON_KEY = ""; // ป้อน Supabase Anon API Key ที่นี่
+const SUPABASE_URL = "https://douygxbizmkcbonmewbw.supabase.co"; // ป้อน Supabase Project URL ที่นี่ เช่น "https://xxxx.supabase.co"
+const SUPABASE_ANON_KEY = "sb_publishable_0xFDmwYwu5XQo1uOQ8F73A_yolkhpYh"; // ป้อน Supabase Anon API Key ที่นี่
 
 // API Key สำหรับ Google AI Studio เพื่อใช้งานผู้ช่วยสักลายอัจฉริยะ (Gemini AI)
 const GEMINI_API_KEY = "AQ.Ab8RN6L1kcgozYDLD7Aga7P9pcA7ShKjhwKM9P1NfPon2sswfQ"; // ป้อน Google AI Studio API Key ที่นี่ เช่น "AIzaSy..."
